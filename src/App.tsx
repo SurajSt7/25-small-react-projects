5c067cd12cc02fa22c7c501c317f4e1c37bcc60a
SurajSt7 <surajst734@gmail.com>
2024-02-15 20:06:35 +0530

    Star rating problem solved with fine CSS

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Accordion from "./Components/Accordion/index.tsx";
 import RandomColors from "./Components/random-colors/index.tsx";
+import StarRating from "./Components/star-rating/index.tsx";
 
 const App = () => {
   return (
@@ -12,7 +13,10 @@ const App = () => {
 
       {/* Random Color Generator Component */}
 
-      <RandomColors />
+      {/* <RandomColors /> */}
+
+      {/* Star Rating Component */}
+      <StarRating noOfStars={5} />
     </div>
   );
 };
--- src/Components/star-rating/index.tsx
+++ src/Components/star-rating/index.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-const StarRating = () => {
+import "./style.css";
+
+type StarRatingProps = {
+  noOfStars: number;
+};
+const StarRating = (prop: StarRatingProps) => {
+  const { noOfStars = 5 } = prop;
+  const [hover, setHover] = useState<number>(0);
+  const [star, setStar] = useState<number>(0);
+
   return (
     <div>
-      <div></div>
+      {[...Array(noOfStars)].map((_, index) => {
+        index += 1;
+        return (
+          <div
+            key={index}
+            className={index <= (hover || star) ? "active" : "inactive"}
+            onMouseOver={() => setHover(index)}
+            onMouseLeave={() => setHover(star)}
+            onClick={() => setStar(index === star ? 0 : index)}
+          >
+            &#9733;
+          </div>
+        );
+      })}
     </div>
   );
 };
